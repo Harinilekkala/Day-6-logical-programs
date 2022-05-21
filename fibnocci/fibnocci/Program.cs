9afcd38dfaa7ed791258de801c449d0a4e02b0ae
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-21 09:39:02 +0530

    [Harini]: program for fibonacci

---
+++ fibnocci/fibnocci/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("1.fibnacci series\n");
+Console.WriteLine("Enter Your Choice:");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+switch (choice)
+{
+    case 1:
+        fibnocci.fibseries fs = new fibnocci.fibseries();
+        fs.series();
+        break;
+}
