2f8588777c40588a0adb1d70f5198d59c99645b4
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-21 19:04:54 +0530

    [Harini]: program for stopwatch

--- fibnocci/fibnocci/Program.cs
+++ fibnocci/fibnocci/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("1.fibnacci series\n2.perfect number\n3.prime number\n4. reverse of a number using while loop\n5. Coupon Number\n");
+Console.WriteLine("1.fibnacci series\n2.perfect number\n3.prime number\n4. reverse of a number using while loop\n5. Coupon Number\n6. Stopwatch \n7. --------------unit testing programs------------------\n");
 Console.WriteLine("Enter Your Choice:");
 int choice = Convert.ToInt32(Console.ReadLine());
 
@@ -29,5 +29,15 @@ switch (choice)
         fibnocci.CouponNumber cp = new fibnocci.CouponNumber();
         cp.coupon();
         break;
+
+    case 6:
+        fibnocci.stopwatchprogram stop = new fibnocci.stopwatchprogram();
+        stop.calculatetime();
+        break;
+
+     
+
+
+
 
 }
---
+++ fibnocci/fibnocci/unittesting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibnocci
+{
+    internal class unittesting
+    {
+    public void testing()
+        {
+            Console.WriteLine("_____________________________________________________________");
+            Console.WriteLine("Part 2 Unit testing Programs");
+        }
+    
+    
+    
+    
+    }
+}
